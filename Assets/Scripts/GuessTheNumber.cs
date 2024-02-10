888ca5fea7298b1d0e1809788c704c4c397ce498
Alastair Feille <me@alastair.se>
2024-02-09 22:53:58 -0600

    Added UI

---
+++ Assets/Scripts/GuessTheNumber.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class GuessTheNumber : MonoBehaviour
+{
+    TMP_Text header;
+    TMP_InputField inputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
