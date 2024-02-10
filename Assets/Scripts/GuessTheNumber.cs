3600290cbdb9dc06bee2a5a915fda0d6493475b0
Alastair Feille <me@alastair.se>
2024-02-09 23:04:05 -0600

    Added GameSetup function

--- Assets/Scripts/GuessTheNumber.cs
+++ Assets/Scripts/GuessTheNumber.cs
@@ -6,15 +6,27 @@ public class GuessTheNumber : MonoBehaviour
     TMP_Text header;
     TMP_InputField inputField;
 
+    int numberOfAttemptsRemaining = 3;
+    int correctAnswer = 0;
+    int guess = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        GameSetup();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void GameSetup()
+    {
+        numberOfAttemptsRemaining = 3;
+        correctAnswer = Random.Range(1, 10 + 1);
+        header.text = $"I'm thinking of a number between 1 and 10. You have {numberOfAttemptsRemaining} attempts to guess it...";
+        inputField.text = string.Empty;
     }
 }
